2f18f688ac4bd7106725e2caea5117423b091cb6
Bernd Holzmüller <bernd.holzmueller@tiggerswelt.net>
2017-11-26 19:31:02 +0100

    Added getters and setters to reject-message

--- Message/Reject.php
+++ Message/Reject.php
@@ -18,6 +18,65 @@
     /* Extra-Data depending on code */
     private $Extra = '';
     
+    // {{{ getCode
+    /**
+     * Retrive the reject-code
+     * 
+     * @access public
+     * @return int
+     **/
+    public function getCode () {
+      return $this->Code;
+    }
+    // }}}
+    
+    // {{{ setCode
+    /**
+     * Set the reject-code for this payload
+     * 
+     * @param int $Code
+     * 
+     * @access public
+     * @return bool
+     **/
+    public function setCode ($Code) {
+      if (($Code < 0) || ($Code > 255))
+        return false;
+      
+      $this->Code = (int)$Code;
+      
+      return true;
+    }
+    // }}}
+    
+    // {{{ getReason
+    /**
+     * Retrive the reason-text
+     * 
+     * @access public
+     * @return string
+     **/
+    public function getReason () {
+      return $this->Reason;
+    }
+    // }}}
+    
+    // {{{ setReason
+    /**
+     * Set reason-text for this payload
+     * 
+     * @param string $Reason
+     * 
+     * @access public
+     * @return bool
+     **/
+    public function setReason ($Reason) {
+      $this->Reason = $Reason;
+      
+      return true;
+    }
+    // }}}
+    
     // {{{ parseData
     /**
      * Try to parse input data
